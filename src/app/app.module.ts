b850a357891a7e5e088642dc87f617c186b790cd
marinashafiq <Marinashafiq@users.noreply.github.com>
2021-02-08 01:12:39 +0200

    add products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,9 @@ import { ProductTracklistingComponent } from "./product-tracklisting/product-tra
 import { ProductListComponent } from "./product-list/product-list.component";
 
 const appRoutes: Routes = [
-  {
-    path: "products",
-    component: ProductListComponent,
-  },
-  {
-    path: "product/:id",
-    component: ProductPageComponent,
-  },
-  {
-    path : "",
-    redirectTo:"products",
-    pathMatch:"full"
-  }
+  {path: "products",component: ProductListComponent},
+  {path: "product/:id",component: ProductPageComponent},
+  {path: "",redirectTo: "products",pathMatch: "full"},
 ];
 
 @NgModule({
@@ -35,10 +25,10 @@ const appRoutes: Routes = [
     ProductListComponent,
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent],
